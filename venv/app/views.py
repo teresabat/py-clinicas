6a4c6e4909b46e76649396e8c4c8d215d435bcc3
TeresaBAT <contatotommymateus@gmail.com>
2024-06-11 14:31:36 -0300

    first commmit

---
+++ venv/app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from app.forms import CarrosForm
+# Create your views here.
+def home(request):
+	return render(request,'index.html')
+
+def form(request):
+	data = {}
+	data['form'] = CarrosForm()
+	return render(request,'form.html', data)
